c6d01dd018423f430f40d8fd7e374b49c9ca74e2
NewPremiumNeo <164292912+NewPremiumNeo@users.noreply.github.com>
2024-03-21 09:26:04 +0530

    Update connectDB.js

--- models/connectDB.js
+++ models/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 ; (async () => {
-  await mongoose.connect(`${process.env.MONGODB_URL}/tempneo`)
+  await mongoose.connect(`${process.env.MONGODB_URL}`)
     .then(() => { console.log('MongoDB Connected'); })
     .catch((err) => { console.log('MongoDB Connection FAILED ', err); });
 })();
