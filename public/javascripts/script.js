68379927686d5f26370d690eda4fab205e8030ed
Neo Fiesta <newytpremium001@gmail.com>
2024-03-23 19:05:19 +0530

    Index edit and bugs fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -70,21 +70,21 @@ gsap.to("#navbar", {
     trigger: "#navbar",
     scroller: "body",
     // markers:true,
-    start: "top -10%",
-    end: "top -11%",
+    start: "top -1vh",
+    end: "top -1.1vh",
     scrub: 1,
   },
 });
 t1.from("#navbar h1, #navbar a", {
-  y:-100,
-  opacity:0,
-  stagger:0.2
+  y: -100,
+  opacity: 0,
+  stagger: 0.2
 })
 
-t1.from(".imp-heading>h1,.btn",{
-  duration:1,
-  opacity:0,
-  scale:0
+t1.from(".imp-heading>h1,.btn", {
+  duration: 1,
+  opacity: 0,
+  scale: 0
 })
 
 
@@ -96,12 +96,12 @@ gsap.from(".person", {
   scrollTrigger: {
     trigger: ".person",
     scroller: "body",
-    scrub: 4,
+    scrub: 1,
   },
 });
 
 gsap.from(".event", {
-  y: 50,
+  y: 100,
   opacity: 0,
   duration: 1.5,
   stagger: 0.4,
@@ -110,6 +110,18 @@ gsap.from(".event", {
     scroller: "body",
   },
 });
+// gsap.from(".cont-event>ui l1", {
+//   x: 100,
+//   opacity: 0,
+//   duration: 1.5,
+//   stagger: 0.4,
+//   scrollTrigger: {
+//     trigger: ".event",
+//     scroller: "body",
+//     markers:true
+//   },
+// });
+
 
 
 
@@ -187,29 +199,72 @@ window.addEventListener("load", function load(event) {
   event.preventDefault();
   //gsap.to(loader, {duration: .5, autoAlpha:  0});
   setTimeout(() => {
-    gsap.to(loader, {autoAlpha: 0, duration:  .3})
-      .then(()=>{
-        loader.style.display="none" 
+    gsap.to(loader, { autoAlpha: 0, duration: .3 })
+      .then(() => {
+        loader.style.display = "none"
       })
   }, 100);
-  
+
   let pageContent = document.getElementsByClassName("page-content")[0];
-  if (pageContent){
-    gsap.fromTo(pageContent, {y: -window.innerHeight / 4}, {y: 0, duration: 1});
+  if (pageContent) {
+    gsap.fromTo(pageContent, { y: -window.innerHeight / 4 }, { y: 0, duration: 1 });
   }
 
-  let tl = new gsap.timeline({paused: true,  onComplete: doneLoading});
+  let tl = new gsap.timeline({ paused: true, onComplete: doneLoading });
   tl.add("start");
   tl.add(gsap.utils.toChars("loading"), "start");
   tl.timeScale(2).progress(0).eventCallback("onUpdate", updateProgressBar, [tl]);
 
   var imgs = document.querySelectorAll(".img-lazyload");
-  for (var i=0;i<imgs.length;i++) {
+  for (var i = 0; i < imgs.length; i++) {
     addLazyLoad(imgs[i], tl);
   };
 
-  var links = document.querySelectorAll ("a[data-link]");
-  for (var j=0;j <links.length;j++){
+  var links = document.querySelectorAll("a[data-link]");
+  for (var j = 0; j < links.length; j++) {
     links[j].addEventListener("click", linkClicked);
   }
 });
+
+
+//Organisers section
+
+function openDialog(role) {
+  const members = {
+    'technical': ['Prince Kumar', 'Ankit Raj'],
+    'management': ['Aaryan', 'Abhishek', 'Akshansh', 'Ankit', 'Divyanshu', 'Hariom', 'Jayanyika', 'Muskan', 'Naina', 'Pavan', 'Prince', 'Raj', 'Rajeev', 'Rishab', 'Riteek', 'Rohit', 'Adarsh', 'Aarya', 'Rohit', 'Saurav', 'Vishal'],
+    'catering': ['Adarsh', 'Raj', 'Rajeev', 'Saurav', 'Hariom'],
+    'stage': ['Naina', 'Jayantika', 'Arya', 'Riteek', 'Akshansh', 'Rishab'],
+    'backstage': ['Ankit'],
+    'finance': ['Prince'],
+    'performance': ['Abhishek', 'Raj', 'Jayantika', 'Muskan', 'Divyanshu'],
+    'decoration': ['Riteek', 'Aaryan', 'Naina']
+    // Add more roles and members as needed
+  };
+
+  const memberList = members[role];
+  const dialogMemberList = document.getElementById('dialogMemberList');
+
+  // Clear previous member list if any
+  dialogMemberList.innerHTML = '';
+
+  // Populate the dialog with members
+  memberList.forEach(member => {
+    const listItem = document.createElement('li');
+    listItem.textContent = member;
+    dialogMemberList.appendChild(listItem);
+    listItem.style.color = '#000';
+  });
+
+  // Display the dialog
+  const dialog = document.getElementById('dialog');
+  dialog.style.display = 'block';
+  document.body.style.overflowY = "hidden";
+}
+
+function closeDialog() {
+  // Hide the dialog
+  const dialog = document.getElementById('dialog');
+  dialog.style.display = 'none';
+  document.body.style.overflowY = "auto";
+}
